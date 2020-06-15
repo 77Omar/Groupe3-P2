b1b86c0398417489570148a26ded43f4edaa55af
77Omar <fayeomzolive@gmail.com>
2020-06-15 12:52:32 +0000

    zo

--- pdo.php
+++ pdo.php
@@ -1,9 +1,9 @@
 <?php
 try
 {
-      //$bdd = new PDO('mysql:host=mysql-omzo.alwaysdata.net;dbname=omzo_bd;charset=utf8', 'omzo', 'omarfaye1234');
+      $bdd = new PDO('mysql:host=mysql-omzo.alwaysdata.net;dbname=omzo_bd;charset=utf8', 'omzo', 'omarfaye1234');
        
-      $bdd = new PDO('mysql:host=localhost;dbname=quizz;charset=utf8', 'root', '');
+      //$bdd = new PDO('mysql:host=localhost;dbname=quizz;charset=utf8', 'root', '');
        
 }
 catch(Exception $e)
